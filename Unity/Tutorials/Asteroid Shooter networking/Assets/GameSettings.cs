34385fa091e1c28edd899752cd02690a03fe906a
Ext3rmin4tor <fdigiacom@gmail.com>
2015-10-07 13:50:02 +0200

    Added asteroid shooter for the networking

---
+++ Unity/Tutorials/Asteroid Shooter networking/Assets/GameSettings.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSettings : MonoBehaviour {
+
+    public static int NumberOfPlayers;
+    public static int PortNumber;
+    public static bool isHost;
+    public static int IPAddress;
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
---
+++ Unity/Tutorials/Asteroid Shooter networking/Assets/UnityShip.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using Lidgren.Network;
+
+public class UnityShip : MonoBehaviour {
+
+    bool hit;
+	public static UnityShip Instantiate(Vector3 pos)
+    {
+        GameObject ship = GameObject.Instantiate(Resources.Load("Ship"), pos, Quaternion.identity) as GameObject;
+        ship.GetComponent<UnityShip>().hit = false;
+        return ship.GetComponent<UnityShip>();
+    }
+
+    public Vector3 Position
+    {
+        get { return this.gameObject.transform.position; }
+        set { this.gameObject.transform.position = value; }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Asteroid")
+        {
+            hit = true;
+        }
+    }
+
+    public bool Hit
+    {
+        get { return hit; }
+        set { hit = value; }
+    }
+
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
