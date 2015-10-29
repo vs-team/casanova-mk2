b16e02364de8031b5a45ad6b9dc8713a7f06edee
mabbadi <mabbadi@outlook.com>
2015-10-29 10:44:52 +0100

    New assets 2

---
+++ Unity/Tutorials/- Galaxy Wars/Assets/UnityArrow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityArrow : MonoBehaviour {
+
+
+  public MeshRenderer arrow_renderer;
+
+	// Use this for initialization
+	void Start () {
+    arrow_renderer = this.gameObject.GetComponentInChildren<MeshRenderer>();
+  }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+                                                                                                                                                                                                                                                                                                                                                                                              
---
+++ Unity/Tutorials/- Galaxy Wars/Assets/UnityEarth.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityEarth : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	public Vector2 ScrollingVelocity = Vector2.zero;
+
+  // Update is called once per frame
+  void Update () {
+	  transform.Rotate(ScrollingVelocity * Time.deltaTime);
+
+  }
+}
+                                                                                                                                                                                                           
